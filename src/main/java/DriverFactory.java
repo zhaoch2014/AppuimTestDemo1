b2376b7f987deb90dea37091fee757c1d3e28bd3
zhaoch <zhaoch2014@126.com>
2020-11-24 19:13:53 +0800

    mid commit

--- src/main/java/DriverFactory.java
+++ src/main/java/DriverFactory.java
@@ -6,17 +6,39 @@ import java.net.MalformedURLException;
 import java.net.URL;
 
 public class DriverFactory {
+
     public static AppiumDriver  getInstance() throws MalformedURLException {
+        AppiumDriver<MobileElement> dirvier = null;
         DesiredCapabilities cap = new DesiredCapabilities();
         cap.setCapability("paltformName","Android");
         cap.setCapability("udid","ed687764");
         cap.setCapability("appPackage","com.tencent.wework");
-        cap.setCapability("appActivity","com.tencent.wework/.launch.LaunchSplashActivity");
+        cap.setCapability("appActivity","com.tencent.wework/.launch.WwMainActivity");
         cap.setCapability("noReset","true");
-        AppiumDriver<MobileElement> dirvier  = new AppiumDriver<MobileElement>(new URL(""), cap);
+        dirvier  = new AppiumDriver<MobileElement>(new URL("http://127.0.0.1:4723/wd/hub"), cap);
         return dirvier;
     }
 
 }
+
+/**
+ *
+ *
+ MobileElement el1 = (MobileElement) driver.findElementById("com.tencent.wework:id/hw4");
+ el1.click();
+ MobileElement el2 = (MobileElement) driver.findElementById("com.tencent.wework:id/i63");
+ el2.click();
+ MobileElement el4 = (MobileElement) driver.findElementById("com.tencent.wework:id/i6n");
+ el4.click();
+ MobileElement el5 = (MobileElement) driver.findElementById("com.tencent.wework:id/gpg");
+ el5.sendKeys("鸡毛");
+ MobileElement el6 = (MobileElement) driver.findElementByXPath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.RelativeLayout/android.widget.RelativeLayout/android.widget.FrameLayout/android.widget.ListView/android.widget.RelativeLayout[2]/android.widget.RelativeLayout/android.widget.RelativeLayout/android.widget.RelativeLayout/android.view.ViewGroup/android.widget.TextView[1]");
+ el6.click();
+
+
+ *
+ *
+ *
+ */
 
 
--- src/main/java/WeWorkTestDemo.java
+++ src/main/java/WeWorkTestDemo.java
@@ -1,8 +1,13 @@
+import io.appium.java_client.AppiumDriver;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+
+import java.net.MalformedURLException;
 
 public class WeWorkTestDemo {
     @Test
-    public void setup() {
-
+    public void setup() throws MalformedURLException {
+        AppiumDriver driver = DriverFactory.getInstance();
+        driver.findElements(By.id("hw4"));
     }
 }
